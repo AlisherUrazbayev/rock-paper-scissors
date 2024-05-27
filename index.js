61baacd746c6e511ee61dcc1bd143b70dc5b13a2
Alisher Urazbayev <Alisher1518204240@gmail.com>
2024-05-27 19:46:28 -0400

    Write a function for prompting human choice and initilize starting score values

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const choice = Math.floor(Math.random() * 3);
   switch (choice) {
@@ -12,4 +15,19 @@ function getComputerChoice() {
   }
 }
 
+function getHumanChoice() {
+  let choice = prompt(
+    "Enter your choice: rock, paper or scissors."
+  ).toLowerCase();
+  while (true) {
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+      return choice;
+    } else {
+      choice = prompt("Enter valid choice.").toLowerCase();
+    }
+  }
+}
+
 console.log(getComputerChoice());
+const ans = getHumanChoice();
+console.log(ans);
