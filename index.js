039f13cc6c9f5ea3bdff7f7c91d9dbda12429b28
Alisher Urazbayev <Alisher1518204240@gmail.com>
2024-05-27 18:53:58 -0400

    Write the function for creating computer choice

---
+++ index.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+  const choice = Math.floor(Math.random() * 3);
+  switch (choice) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+    default:
+      break;
+  }
+}
+
+console.log(getComputerChoice());
